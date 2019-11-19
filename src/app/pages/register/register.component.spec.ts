c72ac5cf6a3b190767ab9d7bf071df8d1e0bb3ae
Levi Regan <S00108881@mail.itsligo.ie>
2019-11-19 21:29:31 +0000

    test not working now

--- src/app/pages/register/register.component.spec.ts
+++ src/app/pages/register/register.component.spec.ts
@@ -1,25 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject,
+  tick,
+  fakeAsync
+} from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { RegisterComponent } from "./register.component";
+import { By } from "@angular/platform-browser";
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { PixbayApiService } from "../../services/pixbay-api.service";
 
-import { RegisterComponent } from './register.component';
-
-describe('RegisterComponent', () => {
+describe("RegisterComponent", () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent, ReactiveFormsModule],
+      imports: [FormGroup, ReactiveFormsModule, PixbayApiService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
