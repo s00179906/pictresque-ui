f85b91aba93816c2fa9d9ba58adb55f5009fb327
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-11-26 15:33:54 +0000

    created a search modal component in the components folder.
    deleted previous search modal component that was in root.
    
    app.module.ts: added search modal component to entry components.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { SearchModalComponent } from "../search-modal/search-modal.component";
 
 @Component({
   selector: "app-search",
@@ -12,7 +13,7 @@ export class SearchComponent implements OnInit {
   constructor(private modalService: NgbModal) {}
 
   openSearchModal() {
-    const modalRef = this.modalService.open('ModelcontentComponent', {
+    const modalRef = this.modalService.open(SearchModalComponent, {
       size: "lg"
     });
     modalRef.componentInstance.name = "World";
