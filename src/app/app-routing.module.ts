b29b03c961b4562cd419b0c4c63206f4836f352e
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-12-31 19:37:53 +0000

    app.component.html --> moved navbar here from home page.
    home.component.html --> removed navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { PostDetailsComponent } from './pages/post-details/post-details.component';
-import { CategoryComponent } from './pages/category/category.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { PostDetailsComponent } from "./pages/post-details/post-details.component";
+import { CategoryComponent } from "./pages/category/category.component";
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'post/:id', component: PostDetailsComponent },
-  { path: 'category/:id', component: CategoryComponent }
-  // { path: '**', component: PageNotFoundComponent }
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "post/:id", component: PostDetailsComponent },
+  { path: "category/:id", component: CategoryComponent }
 ];
 
 @NgModule({
