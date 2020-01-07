bda250aba03544e5d05ff09ca329d49ad662de05
Peter Snee <Peter Snee@DESKTOP-3OF2C5U>
2020-01-07 12:11:26 +0000

    display the pixabay posts

--- src/app/components/pixbayposts/pixbayposts.component.ts
+++ src/app/components/pixbayposts/pixbayposts.component.ts
@@ -1,30 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { PixbayApiService } from 'src/app/services/pixbay-service/pixbay-api.service';
-import { Pixbay } from 'src/app/models/pixbay';
+import { Component, OnInit, Input } from "@angular/core";
+import { PixbayApiService } from "src/app/services/pixbay-service/pixbay-api.service";
 
 @Component({
-  selector: 'app-pixbayposts',
-  templateUrl: './pixbayposts.component.html',
-  styleUrls: ['./pixbayposts.component.scss']
+  selector: "app-pixbayposts",
+  templateUrl: "./pixbayposts.component.html",
+  styleUrls: ["./pixbayposts.component.scss"]
 })
 export class PixbayPostsComponent implements OnInit {
   posts: any = [];
-  searchWord: any;
+  searchWord = "any";
 
   constructor(private apiService: PixbayApiService) {}
 
-  Search = (searchTerm: string): boolean => {
-    console.log('Search Works');
-    this.apiService.getSearchTerm(searchTerm).subscribe(searchWord => {
-      this.searchWord = this.searchWord;
-    });
-    return false;
-  };
-
   ngOnInit() {
-    this.Search('');
-    this.apiService.getImageData().subscribe(hits => {
-      this.posts = hits['hits'];
+    this.apiService.getSearchTerm(this.searchWord).subscribe(hits => {
+      this.posts = hits["hits"];
       console.log(this.posts);
     });
   }
