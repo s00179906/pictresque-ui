180423a12264de5a6bebdc480e1012caad701de8
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2020-01-12 01:05:00 +0000

    filtering by date now works

--- src/app/components/filter-by-date/post-date-filterer.component.ts
+++ src/app/components/filter-by-date/post-date-filterer.component.ts
@@ -3,6 +3,10 @@ import { Store } from "@ngrx/store";
 import { State } from "src/app/store/models/state.model";
 import { map } from "rxjs/operators";
 import { Post } from "src/app/store/models/Post.model";
+import {
+  FilterPostsByDateDescendingAction,
+  FilterPostsByDateAscendingAction
+} from "src/app/store/actions/pictresque.actions";
 
 @Component({
   selector: "app-post-date-filterer",
@@ -10,19 +14,14 @@ import { Post } from "src/app/store/models/Post.model";
   styleUrls: ["./post-date-filterer.component.scss"]
 })
 export class PostDateFiltererComponent implements OnInit {
-  posts: any[];
+  constructor(private _store: Store<State>) {}
 
-  constructor(private _store: Store<State>) {
-    this._store
-      .select(state => state.pictresque.posts)
-      .subscribe(posts => (this.posts = posts));
-
-    this.filterByDate();
+  filterByDateAscending() {
+    this._store.dispatch(new FilterPostsByDateAscendingAction());
   }
 
-  filterByDate() {
-    this.posts = this.posts.sort((a, b) => b.date - a.date);
-    console.log(this.posts);
+  filterByDateDescending() {
+    this._store.dispatch(new FilterPostsByDateDescendingAction());
   }
 
   ngOnInit() {}
